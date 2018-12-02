22f121cf4c9c184dae8523e1e9f683e05b092ffc
Mani Moayedi <acidmanic.moayedi@gmail.com>
2018-12-02 06:59:50 +0330

    Created Empty project

---
+++ SwiftRestful/SwiftRestful.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRestful.h
+//  SwiftRestful
+//
+//  Created by Mani Moayedi on 12/2/18.
+//  Copyright © 2018 Mani Moayedi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftRestful.
+FOUNDATION_EXPORT double SwiftRestfulVersionNumber;
+
+//! Project version string for SwiftRestful.
+FOUNDATION_EXPORT const unsigned char SwiftRestfulVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRestful/PublicHeader.h>
+
+
